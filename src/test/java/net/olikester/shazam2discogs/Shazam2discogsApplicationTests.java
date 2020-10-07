ad453b3d6449b949c7a4e02d7b6e9705044dc832
Oliver Reynolds <oli_kester@zoho.com>
2020-10-07 17:45:44 +0100

    Initial commit. Generated from Spring Initializr

---
+++ src/test/java/net/olikester/shazam2discogs/Shazam2discogsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.olikester.shazam2discogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shazam2discogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
