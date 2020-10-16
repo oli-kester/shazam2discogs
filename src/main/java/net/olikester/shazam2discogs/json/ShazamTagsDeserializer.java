36125778fb72e8680a198f1baa38040d41294e99
Oliver Reynolds <oli_kester@zoho.com>
2020-10-16 21:09:33 +0100

    Now parsing Release information too

--- src/main/java/net/olikester/shazam2discogs/json/ShazamTagsDeserializer.java
+++ src/main/java/net/olikester/shazam2discogs/json/ShazamTagsDeserializer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import net.olikester.shazam2discogs.model.Release;
 import net.olikester.shazam2discogs.model.Tag;
 import net.olikester.shazam2discogs.model.TagList;
 
@@ -21,6 +22,9 @@ public class ShazamTagsDeserializer extends StdDeserializer<TagList> {
     }
 
     @Override
+    /**
+     * Deserialize a full Shazam JSON export into a list of tags, discarding irrelevant data.
+     */
     public TagList deserialize (JsonParser parser, DeserializationContext context) throws IOException {
         TagList tagList = new TagList();
         ObjectCodec codec = parser.getCodec();
@@ -34,6 +38,21 @@ public class ShazamTagsDeserializer extends StdDeserializer<TagList> {
             newTag.setTrackTitle(headers.get("title").asText());
             newTag.setArtist(headers.get("subtitle").asText());
 
+            Release newRelease = new Release();
+            for (JsonNode currFootnote : currTag.get("track").get("footnotes")) {
+                switch (currFootnote.get("title").asText()) {
+                    case "Album":
+                        newRelease.setAlbum(currFootnote.get("value").asText());
+                        break;
+                    case "Label":
+                        newRelease.setLabel(currFootnote.get("value").asText());
+                        break;
+                    case "Released":
+                        newRelease.setReleaseYear(currFootnote.get("value").asText());
+                        break;
+                }
+            }
+            newTag.setRelease(newRelease);
             tagList.add(newTag);
         }
 
--- src/test/java/net/olikester/shazam2discogs/model/TagListTest.java
+++ src/test/java/net/olikester/shazam2discogs/model/TagListTest.java
@@ -6,42 +6,51 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import net.olikester.shazam2discogs.json.ShazamTagsDeserializer;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@SpringBootTest (classes=TagList.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest ( classes = TagList.class )
 class TagListTest {
 
     private static String oneTagJson1 = "";
+    private static ObjectMapper mapper;
 
     @BeforeAll
-    static void setup () throws IOException {
+    public static void setup () throws IOException {
         oneTagJson1 = Files.readString(Path.of("src/test/resources/one-tag-test.json"));
     }
 
-    @Test
-    public void oneTagParseTest () throws JsonProcessingException {
-        Tag expectedTag = new Tag(524476162, "Model Village", "IDLES");
-
-        ObjectMapper mapper = new ObjectMapper();
+    @BeforeEach
+    public void setupEach () {
+        mapper = new ObjectMapper();
         SimpleModule module =
                 new SimpleModule("ShazamTagsDeserializer", new Version(1, 0, 0, null, null, null));
         module.addDeserializer(TagList.class, new ShazamTagsDeserializer());
         mapper.registerModule(module);
-        TagList tags = mapper.readValue(oneTagJson1, TagList.class);
-
-        assertThat(expectedTag).isEqualToComparingFieldByField(tags.toArrayList().get(0));
     }
 
+    /**
+     * Check we can parse a single Shazam tag correctly.
+     */
     @Test
-    public void oneTagParseTestWithRelease () {
-        Tag expectedTag = new Tag(524476162, "Model Village", "IDLES", new Release("Ultra Mono", "Partisan Records", "2020"));
-    }
+    public void oneTagParseTestWithRelease () throws JsonProcessingException {
+        TagList tags = mapper.readValue(oneTagJson1, TagList.class);
+        Tag tag = tags.toArrayList().get(0);
 
+        assertThat(tag).isNotNull();
+        assertThat(tag.getKey()).isEqualTo(524476162);
+        assertThat(tag.getTrackTitle()).isEqualTo("Model Village");
+        assertThat(tag.getArtist()).isEqualTo("IDLES");
+        assertThat(tag.getRelease()).isEqualToComparingFieldByField(
+                new Release("Ultra Mono", "Partisan Records", "2020")
+        );
+    }
 }
