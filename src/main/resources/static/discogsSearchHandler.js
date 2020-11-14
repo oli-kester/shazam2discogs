8ddcd6ef115b19a5e331ada7a55c43b98765cb26
Oliver Reynolds <oli_kester@zoho.com>
2020-11-14 16:43:31 +0000

    Now terminating search progress thread once we finish searching

--- src/main/resources/static/discogsSearchHandler.js
+++ src/main/resources/static/discogsSearchHandler.js
@@ -1,24 +1,26 @@
 // handle search button presses
 document.getElementById("searchForm").addEventListener('submit', (event) => {
   event.preventDefault() // stop the page refreshing straight away. 
+  let searchProgressWorker
 
   // add worker to keep progress bar updated
   if (window.Worker) {
-    const searchProgressWorker = new Worker('searchProgressUpdater.js');
-		searchProgressWorker.onmessage = (event) => {
-	      const progressBar = document.getElementById('searchProgress')
-	      progressBar.setAttribute('aria-valuenow', event.data)
-	      progressBar.setAttribute('style', `width: ${event.data}%`)
-	}
+    searchProgressWorker = new Worker('searchProgressUpdater.js');
+    searchProgressWorker.onmessage = (event) => {
+      const progressBar = document.getElementById('searchProgress')
+      progressBar.setAttribute('aria-valuenow', event.data)
+      progressBar.setAttribute('style', `width: ${event.data}%`)
+    }
   }
 
   // send request to S2D API
   const xhttp = new XMLHttpRequest()
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-	document.open()
-	document.write(xhttp.responseText)
-	document.close()
+      searchProgressWorker.terminate();
+      document.open()
+      document.write(xhttp.responseText)
+      document.close()
     }
   };
   const mediaFormat = document.getElementById('media-type').value
