6ba6835b028f4553aaac123150d9bf95a1b5883e
Oliver Reynolds <oli_kester@zoho.com>
2020-11-15 13:26:49 +0000

    Select all checkbox implemented.

---
+++ src/main/resources/static/toggleAllCheckboxes.js
@@ -0,0 +1,8 @@
+function toggle(source) {
+  const checkboxes = document.getElementsByClassName('tag-select')
+  for(const checkbox of checkboxes) {
+    checkbox.checked = source.target.checked
+  }
+}
+
+document.getElementById('toggleAll').onclick = toggle
