7a897842d26cfda304a2fc227b35e9dc20b20d81
Oliver Reynolds <oli_kester@zoho.com>
2020-12-19 12:58:17 +0000

    Now pulling all the data we need from Shazam's servers without error.

--- src/main/java/com/olikester/shazam2discogs/service/ShazamServiceImpl.java
+++ src/main/java/com/olikester/shazam2discogs/service/ShazamServiceImpl.java
@@ -34,9 +34,29 @@ public class ShazamServiceImpl implements ShazamService {
 	ObjectMapper mapper = new ObjectMapper();
 	try {
 	    JsonNode root = mapper.readTree(response.getBody());
+
 	    if (root.has("images") && root.get("images").has("coverart")) {
 		tag.setImageUrl(root.get("images").get("coverart").asText());
+	    } else {
+		tag.setImageUrl("");
 	    }
+
+	    if (root.has("sections") && root.get("sections").get(0).has("metadata")) {
+		for (JsonNode metadataItem : root.get("sections").get(0).get("metadata")) {
+		    switch (metadataItem.get("title").asText()) {
+		    case "Album":
+			tag.setAlbum(metadataItem.get("text").asText());
+			break;
+		    case "Label":
+			tag.setLabel(metadataItem.get("text").asText());
+			break;
+		    case "Released":
+			tag.setReleaseYear(metadataItem.get("text").asInt());
+			break;
+		    }
+		}
+	    }
+
 	} catch (JsonProcessingException e) {
 	    e.printStackTrace();
 	    return tag;
